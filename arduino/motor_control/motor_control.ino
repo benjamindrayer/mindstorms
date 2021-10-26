29f4eeebcec3d7912ba05d5782cf30055a7c83b0
Benjamin3er <benjamindrayer@gmx.de>
2021-10-26 17:15:48 +0200

    [CMD] - basic sceleton works

--- arduino/motor_control/motor_control.ino
+++ arduino/motor_control/motor_control.ino
@@ -1,12 +1,42 @@
 #define MAX_CMD_LENGTH   128
 
-void setup() {
+void stearMotor1(const char* prt)
+               {
+               }
+void motorStatus1(const char* prt)
+               {
+               }
+
+#define CMD_LIBRARY {{"MOTOR_SET", 2, stearMotor1},\
+                     {"MOTOR_GET", 1, motorStatus1}}
+
+#define CMD_LIBRARY_SIZE  2
+
+typedef void (*myfun)(const char*);
+
+typedef struct
+{
+  const char* pName;
+  int nArguments;
+  myfun pFunction;
+} cliCommand;
+
+cliCommand allCmds[] = CMD_LIBRARY;
+
+void setup() 
+{
   // put your setup code here, to run once:
   Serial.begin(115200);
   while (!Serial) 
   {
     ; // wait for serial port to connect. Needed for Native USB only
   }
+}
+
+
+void motorStatus(const int motorId)
+{
+  Serial.println("Engine status is unknown");  
 }
 
 int readCommand(char* pData, 
@@ -21,20 +51,42 @@ int readCommand(char* pData,
   return readBytes;
 }
 
+int stearMotor(const int motorId,
+               const int speed)
+{
+  return 0;
+}
+
 int processCommand(char* pData, 
                    const int maxLength)
 {
-  String cmdStr = pData;
-  if(cmdStr == "Test")
+  int i = 0;
+  int cmdIndex = -1;
+  while(i<maxLength)
   {
-    Serial.println("CMD: Test");
+    if(pData[i] == ' ')
+    {
+      for(int j=0;j<CMD_LIBRARY_SIZE;j++)
+      {
+        Serial.println(allCmds[j].pName);
+        if(strlen(allCmds[j].pName) == i)
+        {
+          Serial.println("CMD: Found");
+          cmdIndex = j;
+          break;
+        }
+      }
+      break;
+    }
+    i++;
   }
-  else
+  if(cmdIndex==-1)
   {
-    Serial.println("CMD: unknown");
+    Serial.println("CMD: Not Found");    
   }
   return 0;
 }
+
 void loop() 
 {
   char aCmd[MAX_CMD_LENGTH];
