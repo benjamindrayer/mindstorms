e255e3d86fafd3df35615752c15574169dcf48bf
Benjamin3er <benjamindrayer@gmx.de>
2021-10-27 16:37:31 +0200

    [MOTOR] - config                  - LEDS look nice                  - CLI read motor status

--- arduino/motor_control/src/motor_control.c
+++ arduino/motor_control/src/motor_control.c
@@ -1,28 +1,19 @@
 #include "motor_control.h"
 #include "tools.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 #define MAX_MOTORS 3
 MOTOR_Motor_t motors[MAX_MOTORS];
-
-void MOTOR_init(void)
-{
-    for(int i = 0 ; i<MAX_MOTORS; i++)
-    {
-        motors[i].id = i;
-        motors[i].speed = 0;
-        motors[i].position = 0;
-    }
-}
 
 void MOTOR_config(int motor_id,
                   int pinEnable,
                   int pin1,
                   int pin2)
 {
-    motors[motor_id].enablePin = pinEnable;
-    motors[motor_id].input1Pin = pin1;
-    motors[motor_id].input2Pin = pin2;
+    motors[motor_id].config.pinEnable = pinEnable;
+    motors[motor_id].config.pinInput0 = pin1;
+    motors[motor_id].config.pinInput1 = pin2;
 }
 
 void MOTOR_set_speed(int motor_id, 
@@ -33,21 +24,21 @@ void MOTOR_set_speed(int motor_id,
         motors[motor_id].speed = speed;
         if(speed<0)
         {
-            TOOL_writeDigital(motors[motor_id].input1Pin, 1);
-            TOOL_writeDigital(motors[motor_id].input2Pin, 0);
-            TOOL_writeAnalog(motors[motor_id].enablePin, -speed);
+            TOOL_writeDigital(motors[motor_id].config.pinInput0, 1);
+            TOOL_writeDigital(motors[motor_id].config.pinInput1, 0);
+            TOOL_writeAnalog(motors[motor_id].config.pinEnable, -speed);
         }
         if(speed>0)
         {
-            TOOL_writeDigital(motors[motor_id].input1Pin, 0);
-            TOOL_writeDigital(motors[motor_id].input2Pin, 1);
-            TOOL_writeAnalog(motors[motor_id].enablePin, speed);
+            TOOL_writeDigital(motors[motor_id].config.pinInput0, 0);
+            TOOL_writeDigital(motors[motor_id].config.pinInput1, 1);
+            TOOL_writeAnalog(motors[motor_id].config.pinEnable, speed);
         }
         if(speed==0)
         {
-            TOOL_writeDigital(motors[motor_id].input1Pin, 0);
-            TOOL_writeDigital(motors[motor_id].input2Pin, 0);
-            TOOL_writeAnalog(motors[motor_id].enablePin, 0);
+            TOOL_writeDigital(motors[motor_id].config.pinInput0, 0);
+            TOOL_writeDigital(motors[motor_id].config.pinInput1, 0);
+            TOOL_writeAnalog(motors[motor_id].config.pinEnable, 0);
         }
     }
 }
@@ -61,7 +52,20 @@ void CLI_MOTOR_set(const char* pArguments)
     MOTOR_set_speed(atoi(aArgument0), atoi(aArgument1));
 }
 
+void MOTOR_print(int motor_id)
+{
+    char aBuffer[40];
+    sprintf(aBuffer, "Motor :%i\n", motor_id);
+    TOOL_print(aBuffer);
+    sprintf(aBuffer, " -speed: %i\n", motors[motor_id].speed);
+    TOOL_print(aBuffer);
+    sprintf(aBuffer, " -position: %i\n", motors[motor_id].currentPosition);
+    TOOL_print(aBuffer);
+}
+
 void CLI_MOTOR_get(const char* pArguments)
 {
-    
+    char aArgument0[10];
+    int len0 = TOOL_parseArgument(pArguments, aArgument0);    
+    MOTOR_print(atoi(aArgument0));
 }
--- arduino/motor_control/src/motor_control.h
+++ arduino/motor_control/src/motor_control.h
@@ -1,11 +1,23 @@
 typedef struct
 {
+    //Outputs
+    int pinEnable;  ///< Pin to control the power of the motor (PWM)
+    int pinInput0;  ///< Pin0 and Pin1 of the H-Bridge, 01 -> FWD 
+    int pinInput1;  ///<                                10 -> BACK
+                    ///<                                00 -> STOP
+                    ///<                                11 -> STOP
+    //Inputs
+}
+MOTOR_MotorConfig_t;
+
+typedef struct
+{
     int id;
     int speed;
-    int position;
-    int enablePin;  //PWN
-    int input1Pin;  //H-Bridge
-    int input2Pin;  //H-Bridge
+    int currentPosition;    //
+    int targetPosition;     //
+    int direction;          //direction
+    MOTOR_MotorConfig_t config;     //PinConfiguration
 }
  MOTOR_Motor_t;
 
@@ -15,16 +27,16 @@ void MOTOR_config(int motor_id,
                   int pin2);
 
 void CLI_MOTOR_set(const char* pArguments);
-#define CLI_MOTOR_SET_CMD_NAME "MOTOR_SET"
+#define CLI_MOTOR_SET_CMD_NAME "motor_set"
 #define CLI_MOTOR_SET_CMD_HELP "\
-MOTOR_SET [motor_id] [speed] sets the speed of the motor with motor_id\n \
+motor_set [motor_id] [speed] sets the speed of the motor with motor_id\n \
           motor_id: 0,...,n\n \
           speed: -100,...,100\n"
 #define CLI_MOTOR_SET_CMD_NARGS 2
 
 void CLI_MOTOR_get(const char* pArguments);
-#define CLI_MOTOR_GET_CMD_NAME "MOTOR_GET"
+#define CLI_MOTOR_GET_CMD_NAME "motor_get"
 #define CLI_MOTOR_GET_CMD_HELP "\
-MOTOR_GET [motor_id] gets the status of the motor with motor_id\n \
+motor_get [motor_id] gets the status of the motor with motor_id\n \
           motor_id: 0,...,n\n"
 #define CLI_MOTOR_GET_CMD_NARGS 1
